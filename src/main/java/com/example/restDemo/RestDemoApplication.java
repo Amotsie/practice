04ee4eee337fef3724d94b426939807b66eca594
Andreas Motsie <motsie.at@gmail.com>
2021-04-08 02:16:47 +0300

    first commit??

---
+++ src/main/java/com/example/restDemo/RestDemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.restDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.restDemo.model.UserModel;
+
+@SpringBootApplication
+@RestController
+public class RestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public ModelAndView hello(Model model) {
+		ModelAndView mv = new ModelAndView("index.html");
+		return mv;
+	}
+	
+	@GetMapping("/submit")
+	public String formsubmit(UserModel user) {
+		return String.format("Hello %s %s", user.getName(),user.getSurname());
+	}
+}
